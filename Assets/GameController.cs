5ecf090fd16d555cfdc0ee51c3b940f6abe6cc37
Fang C <fangchen198@gmail.com>
2017-10-03 18:38:09 -0400

    Adding project 1 part 1

---
+++ Assets/GameController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	float time; //decimal points
+	int myScore; //integers/whole numbers
+	bool printedMessage = false; //true or false conditions
+
+	// Use this for initialization
+	void Start () {
+		print ("The game has begun!");
+
+		printedMessage = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	//Amount of time that has elapsed in the game
+		if (Time.time > 3 && printedMessage == false) {
+			//stuff that happens if true
+			print ("It has been three seconds!");
+			printedMessage = true;
+		}
+	}
+}
